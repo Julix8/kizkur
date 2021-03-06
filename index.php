1b5c32d987b670b44717475abe882ae82d631403
Julix8 <59317253+Julix8@users.noreply.github.com>
2021-03-06 20:58:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WebMatty</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header class="hero">
+		<nav class="navbar navbar-expand-md navbar-dark">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse drop_menu" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
+					</ul>
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Inicia Sesión</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<section class="container align-items-center ">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+				</div>
+				<div class="col-lg-6 text-light text-center hero_text h-100">
+					<h1 class="hero_title">Vive la experiencia</h1>
+					<p class="hero_body">Disfruta de la magia del scraapbook, la libertad del baile, el frescor de la aventura . . .</p>
+					<button class="btn btn-dark w-50 main_button">Echa a volar</button>
+				</div>
+			</div>
+		</section>
+	</header>
+
+
+		<section class="sec1">
+			<div class="ms-auto me-auto text-white">
+				Holaaaa
+			</div>
+		</section>
+		
+	
+</body>
+</html>
